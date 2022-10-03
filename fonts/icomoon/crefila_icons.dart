cec98a24e38fb0d389103da18b8c4f9b3263e471
iamafasha <afashaisakiye@gmail.com>
2022-10-03 20:11:35 +0300

    Add fonts

---
+++ fonts/icomoon/crefila_icons.dart
@@ -0,0 +1,36 @@
+// Place fonts/crefila.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: crefila
+//      fonts:
+//       - asset: fonts/crefila.ttf
+import 'package:flutter/widgets.dart';
+
+class Crefila {
+  Crefila._();
+
+  static const String _fontFamily = 'crefila';
+
+  static const IconData cart = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData bluetooth = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData search = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData pause = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData play = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData play_outline = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData replay = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData replay_10 = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData replay_30 = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData stop = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData library = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData snapchat = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xf015, fontFamily: _fontFamily);
+  static const IconData tiktok = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData headphones = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData ellipsis_h = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData ellipsis_v = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData facebook = IconData(0xea90, fontFamily: _fontFamily);
+  static const IconData instagram = IconData(0xea92, fontFamily: _fontFamily);
+  static const IconData twitter = IconData(0xea96, fontFamily: _fontFamily);
+  static const IconData youtube = IconData(0xea9d, fontFamily: _fontFamily);
+}
