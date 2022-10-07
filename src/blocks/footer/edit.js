5083e75ee8de799a9fe1492c169f8324fbe43001
iamafasha <afashaisakiye@gmail.com>
2022-10-07 19:27:52 +0300

    Update footer code

--- src/blocks/footer/edit.js
+++ src/blocks/footer/edit.js
@@ -5,9 +5,11 @@ import './editor.scss';
 export default function Edit() {
 	return (
 		<footer { ...useBlockProps() }>
-			<div class="w-full flex bg-sky-900 p-8 text-neutral-200 border-t-2 border-t-gray-400">
+			<div class="w-full flex bg-sky-900 p-8 text-neutral-200 border-t-2 border-t-gray-400 justify-center">
+			<div class="md:w-70%">
 				@ CREFILA UG PROPERTY, 2022
 			</div>
+		</div>
 		</footer>
 	);
 }
--- src/blocks/footer/save.js
+++ src/blocks/footer/save.js
@@ -4,8 +4,10 @@ export default function save() {
 	const year =  new Date().getFullYear();
 	return (
 		<footer { ...useBlockProps.save() }>
-			<div class="w-full flex bg-sky-900 p-8 text-neutral-200 border-t-2 border-t-gray-400">
-				@ CREFILA UG PROPERTY, 2022
+			<div class="w-full flex bg-sky-900 p-8 text-neutral-200 border-t-2 border-t-gray-400 justify-center">
+				<div class="md:w-70%">
+					@ CREFILA UG PROPERTY, 2022
+				</div>
 			</div>
 		</footer>
 	);
